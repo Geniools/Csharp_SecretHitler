832593c75abfc4f921d0c584331d3820e8317f9d
Corvin Wittmaack <corvin.wittmaack@student.nhlstenden.com>
2023-05-26 16:42:21 +0200

    added new fonts & fixed front end

--- SecretHitlerClient/MauiProgram.cs
+++ SecretHitlerClient/MauiProgram.cs
@@ -19,6 +19,8 @@ public static class MauiProgram
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("Plain-Germanica.ttf", "Germanica");
+                fonts.AddFont("Shadowed-Germanica.ttf", "GermanicaBold");
             })
             .Services
             .AddViewModels()
