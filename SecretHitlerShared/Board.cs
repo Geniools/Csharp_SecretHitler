f8895a64c531d8a032bc8a967da2b12daa0c93be
TheLobster1 <rjeveldman@hotmail.nl>
2023-06-23 14:52:28 +0200

    Added shuffle function for the draw deck

--- SecretHitlerShared/Board.cs
+++ SecretHitlerShared/Board.cs
@@ -1,12 +1,13 @@
 ﻿
 using System.Collections;
 using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
 
 namespace SecretHitlerShared
 {
     public class Board
     {
-        private readonly HashSet<PolicyCard> _drawDeck;
+        private Stack<PolicyCard> _drawDeck;
         public byte PlayedLiberalCards { get; private set; }
         public byte PlayedFascistsCards { get; private set; }
 
@@ -16,7 +17,7 @@ namespace SecretHitlerShared
 
         public Board()
         {
-            _drawDeck = new HashSet<PolicyCard>();
+            _drawDeck = new Stack<PolicyCard>();
 
             this.LiberalPolicies = new ObservableCollection<PolicyCard>();
             this.FascistPolicies = new ObservableCollection<PolicyCard>();
@@ -29,22 +30,37 @@ namespace SecretHitlerShared
             // 11 fascist policies
             for(byte i = 0; i < 11; i++ )
             {
-                this._drawDeck.Add(new PolicyCard(PartyMembership.Fascist));
+                this._drawDeck.Push(new PolicyCard(PartyMembership.Fascist));
             }
 
             // 6 liberal policies
             for(byte i = 0; i < 6; i++ )
             {
-                this._drawDeck.Add(new PolicyCard(PartyMembership.Liberal));
+                this._drawDeck.Push(new PolicyCard(PartyMembership.Liberal));
             }
 
             // Shuffle the deck
-            //this.ShuffleDeck();
+            this.ShuffleDeck();
+        }
+
+        private void ShuffleDeck()
+        {
+            PolicyCard[] cards = this._drawDeck.ToArray(); 
+            Random random = new Random();
+            int n = cards.Length;
+            for( int i = 0; i < (n - 1); i++)
+            {
+                int r = i + random.Next(n - i);
+                PolicyCard card = cards[r];
+                cards[r] = cards[i];
+                cards[i] = card;
+            }
+            this._drawDeck = new Stack<PolicyCard>(cards);
         }
 
         public PolicyCard DrawNextPolicy()
         {
-            throw new NotImplementedException();
+            return this._drawDeck.Pop();
         }
 
         private void EnactLiberalPolicy()
