aecd059d0ccd059a222efdc4ae873e8050aaea5c
Geniools <30665942+Geniools@users.noreply.github.com>
2023-06-28 14:53:13 +0200

    Attempt to run the election

--- SecretHitlerClient/ViewModel/MainPageViewModel.cs
+++ SecretHitlerClient/ViewModel/MainPageViewModel.cs
@@ -211,9 +211,11 @@ namespace SecretHitler.ViewModel
             }
         }
 
-        private void PresidentSelected(Player currentPresident)
+        private async void PresidentSelected(Player currentPresident)
         {
             this.GameManager.PresidentSelected(currentPresident);
+            await Shell.Current.DisplayAlert("President selected", $"{currentPresident.Username} is the new president", "OK");
+
             // If this is the president, change the UI
             if(this.GameManager.SignalRService.ThisPlayer.Equals(currentPresident))
             {
